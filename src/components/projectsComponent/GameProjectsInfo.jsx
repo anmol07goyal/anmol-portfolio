7b353ea0769a0e53527a4b3ecd113c7a18705d39
anmol07goyal <anmol77goyal@gmail.com>
2024-03-14 16:45:21 -0600

    Update GameProjectsInfo.jsx
    updated from drive links to youtube links

--- src/components/projectsComponent/GameProjectsInfo.jsx
+++ src/components/projectsComponent/GameProjectsInfo.jsx
@@ -5,28 +5,28 @@ const GameProjectsInfo = {
       shortDesc:
         "Dive into our dynamic 2D platformer game set in the world of cybersecurity. Experience secure login, real-time chat, and competitive challenges. Explore optimized gameplay, leaderboards, and personalized experiences. Elevate your journey in this compact yet thrilling cyberwar platformer.",
       codeLink: "#",
-      demoLink: "https://drive.google.com/file/d/1adP9dDYEChJTQy96rpuL4MB3d4gaoEty/view?usp=sharing",
+      demoLink: "https://youtu.be/MV36gjVK3cw",
     },
     Learnfest: {
       name: "Kompanions Learnfest",
       shortDesc:
         "An online learning tool that allows students to study a variety of science, math, and other disciplines at different levels through fresh, dependable, interactive 2D and 3D simulations.",
       codeLink: "#",
-      demoLink: "https://drive.google.com/file/d/1Z0Ebcj8z-4V6vistl7fE-CBVGvwVZgem/view?usp=sharing",
+      demoLink: "https://youtu.be/yg3mcVPSB4c",
     },
     PingPongMania: {
       name: "Ping Pong Mania",
       shortDesc:
         "Embark on a thrilling journey within our 2D table tennis game. Primarily focused on the use of animations, specifically spine animations with over 90 levels and an interactive user interface. Facebook API to share the game, optimizing the game to support lower-end devices.",
       codeLink: "#",
-      demoLink: "https://drive.google.com/file/d/1Zcveu1oDTGCKIIJOghpBO0PXbwn_s9Zx/view?usp=sharing",
+      demoLink: "https://youtube.com/shorts/YVASlDso0zA?feature=share",
     },
     SuperHeroRun: {
       name: "Super Hero Run",
       shortDesc:
         "Hyper casual 3d runner game prototype where one has to switch to different characters according to the obstacles in front. Compete concurrently with other AI players to establish your value.",
       codeLink: "#",
-      demoLink: "https://drive.google.com/file/d/1ac4zIYls6t3jHPsusv9XeEi9qNcjzxme/view?usp=sharing",
+      demoLink: "https://youtube.com/shorts/ZI1y73tJfw8?feature=share",
     },
     PhobiaVoyage: {
       name: "VR Phobia Voyage",
@@ -40,7 +40,7 @@ const GameProjectsInfo = {
       shortDesc:
         "An educational game that combines an AR experience which brings the planets to life in a virtual environment with entertaining and beneficial information about the planets.",
       codeLink: "#",
-      demoLink: "https://drive.google.com/file/d/1tFYH-GoA3HVeh3u3awlM4b_rIhmGsZoG/view?usp=sharing",
+      demoLink: "https://youtube.com/shorts/Kb9MhbKSnPU?feature=share",
     },
   },
 };
